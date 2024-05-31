63feb6dabdc2855e864f4f0c08e2586754a6a639
0nnu <142360951+0nnu@users.noreply.github.com>
2024-05-31 06:34:12 +0300

    Update resume.js

--- musicCommands/resume.js
+++ musicCommands/resume.js
@@ -1,14 +1,14 @@
 const playModule = require('./play.js');
 const { VoiceConnectionStatus } = require('@discordjs/voice');
 module.exports = {
-  name: 'resume',
+  name: 'كمل',
   description: 'Resume the paused music playback',
   execute: (message, args) => {
     const currentConnection = playModule.getCurrentConnection();
     if (currentConnection && currentConnection.state.status === VoiceConnectionStatus.Ready) {
       playModule.resume();
     } else {
-      message.reply('❌ The bot is not currently playing any music.');
+      message.reply('❌ يبو البوت مو قاعد يشغل اغاني عشان يكمل بعقلك انت؟.');
     }
   },
 };
